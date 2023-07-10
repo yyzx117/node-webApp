c1ba0ca597cfc4308d85b12f74d4af87ad8ae90d
yyzx117 <97866407+yyzx117@users.noreply.github.com>
2023-07-10 15:36:04 -0400

    update index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+var os = require('os');
+app.get('/', function (req, res) {
+    res.send('Hello World from host ' + os.hostname() + '!')
+})
+
+app.listen(3000, function() {
+    console.log('Hello world app listgening on port 3000!')
+})
