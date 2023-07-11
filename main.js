04d09b5f98790247570b24ef3ab3fb846f316359
yyzx117 <97866407+yyzx117@users.noreply.github.com>
2023-07-11 16:16:23 -0400

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/pain'});
-    response.end('Hello World\n');
-}).listen(8081);
-
-console.log('Server running on port 8081');
