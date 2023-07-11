d9c56ab492befbaa14ce0584fffb8028c6e22060
yyzx117 <97866407+yyzx117@users.noreply.github.com>
2023-07-11 11:43:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
 app.disable("x-powered-by");
 app.get('/', function (req, res) {
     res.send('Hello World!')
 })
 
-app.listen(80, function() {
-    console.log('Hello world app listgening on port 80!')
+app.listen(3000, function() {
+    console.log('Hello world app listgening on port 3000!')
 })
